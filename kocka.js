1f54e0dd00ff8a742258a48cf4bb9a3f405c75c7
pakasz <100175048+pakasz@users.noreply.github.com>
2023-03-27 19:04:40 +0200

    Add files via upload
    dobokocka game with added patches v1.2

--- kocka.js
+++ kocka.js
@@ -1,34 +1,46 @@
 var egyenleg = 10000;
+var hazaviheto = 0;
+var nyeremeny = 0;
 var kockaertek;
 var kor = 1;
 var tesztkor = true;
 var end = false;
 //var form = new FormData(document.getElementById("form"));
 function jatek(){
-    if(!end){
+    if(!end){   
         let tet = document.getElementById("tet").value;
-    
+        if(isNaN(tet) || tet < 100){
+            document.getElementById("vege").innerHTML = "Hibás bemenet! A minimum tét 100, a maximum az egyenleged értéke. Bemenet csak számformátumban!";
+            return;
+        }
+        document.getElementById("vege").innerHTML = " ";
+        kor++
         if(egyenleg && (kor < 3) && tesztkor){
-            document.getElementById("korte").innerHTML = "Tesztkör: "+ kor;
+            document.getElementById("korte").innerHTML = "Tesztkör: " + kor;
             kockaertek = getRandomInt(1,7);
                 if(kockaertek >= 5){
-                    egyenleg = egyenleg/2 + 2*tet;
+                    egyenleg = egyenleg;
+                    nyeremeny = nyeremeny + 2*tet;
+                    hazaviheto = egyenleg/2  + nyeremeny;
                     document.getElementById("szam").innerHTML = "Dobott szám: " +kockaertek;
                     document.getElementById("egyenleg").innerHTML = "Egyenleg: "+egyenleg;
+                    document.getElementById("hazaviheto").innerHTML = "Hazavihető: "+ hazaviheto;
                 }
                 else{
                     egyenleg = egyenleg - tet;
+                    hazaviheto = egyenleg/2  + nyeremeny;
                     document.getElementById("szam").innerHTML = "Dobott szám: " + kockaertek;
                     document.getElementById("egyenleg").innerHTML = "Egyenleg: "+ egyenleg;
+                    document.getElementById("hazaviheto").innerHTML = "Hazavihető: "+ hazaviheto;
                     if(!egyenleg){
                         tesztkor = false;
-                        document.getElementById("vege").innerHTML = "Vége a kettő próbakörnek, mostantól élesben megy!"; 
+                        document.getElementById("vege").innerHTML = "!"; 
                     }   
                 }
             }
             else{
                 tesztkor = false;
-                document.getElementById("vege").innerHTML = "Vége a kettő próbakörnek, mostantól élesben megy!";
+                document.getElementById("vege").innerHTML = " Ön sikeresen lejátszott két próbakört. Amennyiben nem értette meg a játék dinamikáját, kérem frissítse az oldalt olvassa el a tájékoztatót, és játszon le még két próbakört. Amennyiben átlátható a játék kérem lépjen vissza a kérdőívhez és válaszoljon a következő kérdésre.";
             }
         
 
@@ -36,7 +48,9 @@ function jatek(){
 
 
         if(!tesztkor){
-            egyenleg = 10000;
+            egyenleg = 10000;   
+            nyeremeny = 0;
+            hazaviheto = 0;
         }
 
 
@@ -44,15 +58,20 @@ function jatek(){
             document.getElementById("korte").innerHTML = "Kör: "+ (kor-2);
             kockaertek = getRandomInt(1,7);
             if(kockaertek >= 5){
-                egyenleg = egyenleg/2 + 2*tet;
+                egyenleg = egyenleg;
+                nyeremeny = nyeremeny + 2*tet;
+                hazaviheto = egyenleg/2 + nyeremeny;
                 document.getElementById("szam").innerHTML = "Dobott szám: " +kockaertek;
                 document.getElementById("egyenleg").innerHTML = "Egyenleg: "+egyenleg;
+                document.getElementById("hazaviheto").innerHTML = "Hazavihető: "+ hazaviheto;
             }
             else{
                 if(egyenleg-tet >=0){
                     egyenleg = egyenleg - tet;
+                    hazaviheto = egyenleg/2 + nyeremeny;
                     document.getElementById("szam").innerHTML = "Dobott szám: " +kockaertek;
                     document.getElementById("egyenleg").innerHTML = "Egyenleg: "+egyenleg;  
+                    document.getElementById("hazaviheto").innerHTML = "Hazavihető: "+ hazaviheto;
                 }
                 else if(egyenleg-tet == 0){
                     egyenleg = 0;
@@ -60,11 +79,10 @@ function jatek(){
                 }   
             }
         }
-        else if(!egyenleg){
+        else if(!tesztkor){
             document.getElementById("vege").innerHTML = "Vége a játéknak, kérlek mentsd el az elért eredményed!";
             end = true;
         }
-        kor++;
     }
 
 }
